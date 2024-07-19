be98777b428e88ae1d2f0edbbd1679a841a3fef2
kumkum13 <kumkumsharma805@gmail.com>
2024-07-19 15:18:22 +0530

    commit

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const StudentModel = require("../models/Students");
-
+require("dotenv").config();
 passport.use(
   new GoogleStrategy(
     {
