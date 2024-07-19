1b11d30bda579c0fd96da3294456b54e75227cf7
kumkum13 <kumkumsharma805@gmail.com>
2024-07-19 21:05:11 +0530

    crousel

--- routes/Crousel.js
+++ routes/Crousel.js
@@ -1,27 +1,33 @@
 const express = require('express');
+const multer = require('multer');
 const mongoose = require('mongoose');
 const router = express.Router();
-const multer = require('multer');
-const { Readable } = require('stream');
 
 const carouselSchema = new mongoose.Schema({
-  imageData: { type: String, required: true },
+  imageUrl: { type: String, required: true },
   label: { type: String, required: true },
   text: { type: String, required: true },
 });
 
 const CarouselItem = mongoose.model('CarouselItem', carouselSchema);
 
-// Middleware to handle file upload
-const storage = multer.memoryStorage(); // Use memory storage to get file buffer
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'crousel_img/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
 const upload = multer({ storage });
 
 // Route to add new carousel item
 router.post('/add-carousel-item', upload.single('image'), async (req, res) => {
   const { label, text } = req.body;
-  const imageData = req.file.buffer.toString('base64'); // Convert buffer to base64 string
-  const newItem = new CarouselItem({ imageData, label, text });
-  
+  const imageUrl = `https://reactlvbackend.onrender.com/crousel_img/${req.file.filename}`;
+  const newItem = new CarouselItem({ imageUrl, label, text });
   try {
     await newItem.save();
     res.json(newItem);
@@ -44,7 +50,7 @@ router.get('/carousel-items', async (req, res) => {
 
 // Route to delete carousel item by ID
 router.post('/delete-carousel-item', async (req, res) => {
-  const { itemId } = req.body;
+  const  {itemId}  = req.body;
   try {
     const deletedItem = await CarouselItem.findByIdAndDelete(itemId);
     if (!deletedItem) {
