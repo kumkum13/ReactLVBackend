da6a0d047953805a803f7da5657d8773de4d33e0
kumkum13 <kumkumsharma805@gmail.com>
2024-07-19 16:19:31 +0530

    error

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-    res.redirect("http://localhost:3000/");
+    res.redirect("https://reactlvbackend.onrender.com/");
   }
 );
 
